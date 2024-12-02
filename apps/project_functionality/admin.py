23e61444ab87c15050de409d21d45d3fca22a92d
natali <natali.dnp.ua@gmail.com>
2024-12-02 21:22:15 +0200

    add class Section to admon

--- apps/project_functionality/admin.py
+++ apps/project_functionality/admin.py
@@ -10,9 +10,20 @@ class HelpPointAdmin(admin.ModelAdmin):
     list_display = (
         "name",
         "information",
+        "section",
         "created_at",
     )
 
 
 class ContactInline(admin.TabularInline):
     model = models.HelpPoint
+
+
+@admin.register(models.Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name_section",
+        "created_at",
+    )
+    # list_filter = ("name",)
+    inlines = (ContactInline,)
