3dee3b46f92ae4b805c0dcd6120dbdba41c55555
natali <natali.dnp.ua@gmail.com>
2024-12-02 21:21:44 +0200

    add class Section

--- apps/project_functionality/models.py
+++ apps/project_functionality/models.py
@@ -8,10 +8,26 @@ from django.db import models
 #     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class Section(models.Model):
+    name_section = models.CharField(max_length=255)  # Название секции (например, "Одежда")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name_section
+
+
 class HelpPoint(models.Model):
     name = models.TextField(default=None, null=True, blank=True)
     information = models.TextField(default=None, null=True, blank=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    section = models.ForeignKey(
+        Section,
+        on_delete=models.CASCADE,
+        related_name="helppoints",
+        default=None,
+        null=True,
+        blank=True,
+    )  # Связь с секцией
 
     def __str__(self) -> str:
         return f"{self.information}"
