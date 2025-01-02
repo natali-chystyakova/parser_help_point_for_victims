54e350d38a69d150967a7781f69a9dde1a1fcbc6
natali <natali.dnp.ua@gmail.com>
2025-01-02 21:54:41 +0200

    add get_absolute_url and add class Meta for models

--- apps/project_functionality/models.py
+++ apps/project_functionality/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 # class Url(models.Model):
 #     url = models.CharField(max_length=100)
@@ -10,17 +10,26 @@ from django.db import models
 
 class Section(models.Model):
     name_section = models.CharField(max_length=255)  # Название секции (например, "Одежда")
+    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name_section
 
+    def get_absolute_url(self):
+        return reverse("project_functionality:section", kwargs={"sect_id": self.pk})
+
+    class Meta:
+        ordering = ["pk"]
+        verbose_name = "section for points"
+        verbose_name_plural = "sections for points"
+
 
 class HelpPoint(models.Model):
     name = models.TextField(default=None, null=True, blank=True)
-    information = models.TextField(default=None, null=True, blank=True, unique=True)
+    information = models.TextField(default=None, null=True, blank=True, unique=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    section = models.ForeignKey(
+    sect = models.ForeignKey(
         Section,
         on_delete=models.CASCADE,
         related_name="helppoints",
@@ -29,7 +38,13 @@ class HelpPoint(models.Model):
         blank=True,
     )  # Связь с секцией
 
+    class Meta:
+        ordering = ["id"]
+
     def __str__(self) -> str:
-        return f"{self.information}"
+        return f"{self.name}"
 
     __repr__ = __str__
+
+    # def get_absolute_url(self):
+    #     return reverse('project_functionality:list', kwargs={'sect': self.sect})
