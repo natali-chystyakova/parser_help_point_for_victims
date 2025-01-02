3187a43283379cb926c6f72332587f1a2c911bd4
natali <natali.dnp.ua@gmail.com>
2025-01-02 22:03:48 +0200

    add ShowSectionList, ContactForm

--- apps/project_functionality/views.py
+++ apps/project_functionality/views.py
@@ -1,9 +1,13 @@
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from apps.project_functionality.models import HelpPoint
+
+from apps.project_functionality.forms import ContactForm
+from apps.project_functionality.models import HelpPoint, Section
 from apps.project_functionality.tools import paginate_by_condition
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 
 class HelpPointListView(ListView):
@@ -11,7 +15,7 @@ class HelpPointListView(ListView):
     paginate_by = 5
     template_name = "project_functionality/helppoint_list.html"
 
-    queryset = HelpPoint.objects.all().order_by("created_at")
+    queryset = HelpPoint.objects.all().order_by("created_at").select_related("sect")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -31,6 +35,8 @@ class HelpPointListView(ListView):
         context["queries_annotate_all"] = queries_annotate_all
         context["paginated_queryset"] = paginated_queryset
         context["page_obj"] = paginated_queryset
+        context["cat_selected"] = 0
+        context["sections"] = Section.objects.all()
 
         return context
 
@@ -43,8 +49,95 @@ class HelpPointUpdateView(UpdateView):
     )
     success_url = reverse_lazy("project_functionality:list")
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sections"] = Section.objects.all()
+        context["cat_selected"] = self.kwargs.get("sect_id", 0)
+
+        return context
+
 
 class HelpPointDeleteView(DeleteView):
     model = HelpPoint
 
     success_url = reverse_lazy("project_functionality:list")
+    success_url = reverse_lazy("project_functionality:list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sections"] = Section.objects.all()
+        context["cat_selected"] = self.kwargs.get("sect_id", 0)
+
+        return context
+
+
+class SectionListView(ListView):
+    model = Section
+    template_name = "project_functionality/section_list.html"
+
+    queryset = Section.objects.all().order_by("created_at")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data["title"] = "Section"
+
+        return context_data
+
+
+def show_section(request, sect_id):
+    points = HelpPoint.objects.filter(id=sect_id)
+    sections = Section.objects.all()
+
+    if len(points) == 0:
+        raise Http404()
+
+    context = {
+        "points": points,
+        "sections": sections,
+        "title": "Отображение по рубрикам",
+        "cat_selected": sect_id,
+    }
+
+    # return render(request, 'project_functionality/section_list.html', context=context)
+    return render(request, "project_functionality/helppoint_list.html", context=context)
+
+
+class ShowSectionListView(ListView):
+    model = HelpPoint
+
+    template_name = "project_functionality/list.html"
+    allow_empty = False  # появление ошибки 404, что страника не найдена
+    # context_object_name = 'points_filter'
+
+    def get_queryset(self):
+        sect_id = self.kwargs.get("sect_id")  # Получаем ID категории из URL
+        return HelpPoint.objects.filter(sect_id=sect_id).order_by("created_at").select_related("sect")
+
+    def get_context_data(self, object_list=None, **kwargs):
+        # Получаем базовый контекст
+        context = super().get_context_data(**kwargs)
+
+        # Добавляем данные секции в контекст
+        context["sections"] = Section.objects.all()
+
+        context["cat_selected"] = self.kwargs.get("sect_id", 0)
+        context["title"] = "Категория"
+
+        return context
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = "project_functionality/contact.html"
+    success_url = reverse_lazy("project_functionality:list")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Зворотнiй зв'язок"
+        context["sections"] = Section.objects.all()
+        return context
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect("project_functionality:list")
