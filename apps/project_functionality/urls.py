f60842d833f88e9324f9f2159f3abde2e9ab3aef
natali <natali.dnp.ua@gmail.com>
2025-01-02 22:00:46 +0200

    add ContactForm and Section to urls

--- apps/project_functionality/urls.py
+++ apps/project_functionality/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 from django.contrib.auth.decorators import login_required
 
+
 app_name = "project_functionality"
 
 urlpatterns = [
     path("list/", views.HelpPointListView.as_view(), name="list"),
     path("update/<int:pk>/", login_required(views.HelpPointUpdateView.as_view()), name="update"),
     path("delete/<int:pk>/", login_required(views.HelpPointDeleteView.as_view()), name="delete"),
+    path("contact/", views.ContactFormView.as_view(), name="contact"),
+    path("sections/", views.SectionListView.as_view(), name="sections_list"),
+    # path('sections/<int:sect_id>/', show_section, name='section'),
+    path("sections/<int:sect_id>/", views.ShowSectionListView.as_view(), name="section"),
 ]
