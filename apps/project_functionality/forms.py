98e89dd359e2b01d83af6e836bfdb29dda6ffca6
natali <natali.dnp.ua@gmail.com>
2025-01-27 15:02:27 +0200

    add form back-contact

--- apps/project_functionality/forms.py
+++ apps/project_functionality/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from captcha.fields import CaptchaField
 
 
+# class ContactForm(forms.Form):
+#     name = forms.CharField(label="Iм'я", max_length=225)
+#     email = forms.EmailField(label="Email")
+#     content = forms.CharField(widget=forms.Textarea(attrs={"cols": 60, "rows": 10}))
+#
+#     captcha = CaptchaField()
+
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Iм'я", max_length=225)
-    email = forms.EmailField(label="Email")
-    content = forms.CharField(widget=forms.Textarea(attrs={"cols": 60, "roes": 10}))
-    capatcha = CaptchaField()
+    name = forms.CharField(label="Iм'я", max_length=225, widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "cols": 60, "rows": 10}))
+    captcha = CaptchaField()
